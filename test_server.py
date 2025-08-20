418a88c47fcc382576cdaf2f96d87a26bace90dc
tjdudfhr <genspark_dev@genspark.ai>
2025-08-20 02:46:12 +0000

    fix: 법령 ID 기반 정확한 국가법령정보센터 링크 매칭
    - 102개 법규의 국가법령정보센터 링크를 법령ID(lsId) 기반으로 정확하게 매칭
    - 3번째 탭(법규 레지스트리)의 법규 클릭 시 올바른 법령 페이지로 연결되도록 수정
    - showLawDetail 함수의 법령 링크도 동일하게 개선
    - 시행일자(efYd) 파라미터 추가로 더 정확한 법령 버전 표시
    - 법령ID가 없는 경우 fallback으로 제목 검색 링크 제공

---
+++ test_server.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+import http.server
+import socketserver
+import os
+
+PORT = 8080
+DIRECTORY = "docs"
+
+class MyHTTPRequestHandler(http.server.SimpleHTTPRequestHandler):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, directory=DIRECTORY, **kwargs)
+    
+    def end_headers(self):
+        self.send_header('Access-Control-Allow-Origin', '*')
+        self.send_header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS')
+        self.send_header('Access-Control-Allow-Headers', 'Content-Type')
+        super().end_headers()
+
+with socketserver.TCPServer(("0.0.0.0", PORT), MyHTTPRequestHandler) as httpd:
+    print(f"Server running at http://0.0.0.0:{PORT}/")
+    print(f"Serving directory: {DIRECTORY}")
+    httpd.serve_forever()
